df68a8c996f4ccc4868237e05dec179b2760f85f
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-07-25 15:55:42 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,6 +4,7 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
   
   def actors_list 
+    binding.pry
     self.characters each do |character|
       "#{character.actor}"
     end 
