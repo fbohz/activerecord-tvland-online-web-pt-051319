4994700b07361bd42b4d03cb6aa08c9a73fd6a4a
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-07-25 15:43:59 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -5,12 +5,5 @@ class Character < ActiveRecord::Base
   def say_that_thing_you_say
     "#{self.name} always says: #{self.catchphrase}"
   end 
-  
-  # def build_show(name:)
-    
-  # end 
-  
-  # def build_network(call_letters:)
-    
-  # end 
+
 end
