dc510c98e18a222f3c5d2c026ddf124e6653d594
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-07-25 16:02:50 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,7 +4,6 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
   
   def actors_list 
-   # binding.pry
     self.actors.collect do |actor|
       actor.full_name
     end 
